e9ba783502d07baa719c19f46cbf577e18dc8f2f
Michal Fularz <michal.fularz@gmail.com>
2014-11-05 21:45:24 +0100

    Added simple USART test task and I2C test task. A lot of cleaning up still required.

--- expansion_board.c
+++ expansion_board.c
@@ -6,6 +6,7 @@
 #include "stm32f4xx_dac.h"
 #include "stm32f4xx_adc.h"
 #include "stm32f4xx_usart.h"
+#include "stm32f4xx_i2c.h"
 
 #include "expansion_board_led.h"
 #include <math.h>
@@ -55,6 +56,8 @@ void vExpansionBoardButton3Task(void* pvparameters);
 void vExpansionBoardButton4Task(void* pvparameters);
 void vExpansionBoardRGBLedFader(void* pvparameters);
 void vExpansionBoardDACTask(void* pvparameters);
+void vExpansionBoardUSARTTask(void* pvparameters);
+void vExpansionBoardI2CTemperatureTask(void* pvparameters);
 
 /*
  * When FreeRTOS crashes, you often end up in a hard fault.
@@ -95,6 +98,10 @@ void EXPANSION_BOARD_test(void)
 
 	xTaskCreate( vExpansionBoardRGBLedFader, "RGB fader", 200, NULL, 1, NULL );
 
+	xTaskCreate( vExpansionBoardUSARTTask, "USART sender", 100, NULL, 1, NULL );
+
+	xTaskCreate( vExpansionBoardI2CTemperatureTask, "I2C sensor", 100, NULL, 1, NULL );
+
 
 	//xTaskCreate( vExpansionBoardDACTask, "DAC dma", 200, NULL, 1, NULL );
 
@@ -111,6 +118,273 @@ void EXPANSION_BOARD_test(void)
     		asm("nop");
     	}
     }
+}
+
+/* This function issues a start condition and
+* transmits the slave address + R/W bit
+*
+* Parameters:
+* I2Cx --> the I2C peripheral e.g. I2C1
+* address --> the 7 bit slave address
+* direction --> the tranmission direction can be:
+* I2C_Direction_Tranmitter for Master transmitter mode
+* I2C_Direction_Receiver for Master receiver
+*/
+void I2C_start(I2C_TypeDef* I2Cx, uint8_t address, uint8_t direction)
+{
+	// wait until I2C1 is not busy anymore
+	while(I2C_GetFlagStatus(I2Cx, I2C_FLAG_BUSY));
+	// Send I2C1 START condition
+	I2C_GenerateSTART(I2Cx, ENABLE);
+	// wait for I2C1 EV5 --> Slave has acknowledged start condition
+	while(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_MODE_SELECT));
+	// Send slave Address for write
+	I2C_Send7bitAddress(I2Cx, address, direction);
+	/* wait for I2C1 EV6, check if
+	* either Slave has acknowledged Master transmitter or
+	* Master receiver mode, depending on the transmission
+	* direction
+	*/
+	if(direction == I2C_Direction_Transmitter)
+	{
+		while(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
+	}
+	else if(direction == I2C_Direction_Receiver)
+	{
+		while(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED));
+	}
+}
+/* This function transmits one byte to the slave device
+* Parameters:
+* I2Cx --> the I2C peripheral e.g. I2C1
+* data --> the data byte to be transmitted
+*/
+void I2C_write(I2C_TypeDef* I2Cx, uint8_t data)
+{
+	I2C_SendData(I2Cx, data);
+	// wait for I2C1 EV8_2 --> byte has been transmitted
+	while(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+}
+/* This function reads one byte from the slave device
+* and acknowledges the byte (requests another byte)
+*/
+uint8_t I2C_read_ack(I2C_TypeDef* I2Cx)
+{
+	// enable acknowledge of recieved data
+	I2C_AcknowledgeConfig(I2Cx, ENABLE);
+	// wait until one byte has been received
+	while( !I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_BYTE_RECEIVED) );
+	// read data from I2C data register and return data byte
+	uint8_t data = I2C_ReceiveData(I2Cx);
+	return data;
+}
+/* This function reads one byte from the slave device
+* and doesn't acknowledge the recieved data
+*/
+uint8_t I2C_read_nack(I2C_TypeDef* I2Cx)
+{
+	// disabe acknowledge of received data
+	I2C_AcknowledgeConfig(I2Cx, DISABLE);
+	// wait until one byte has been received
+	while( !I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_BYTE_RECEIVED) );
+	// read data from I2C data register and return data byte
+	uint8_t data = I2C_ReceiveData(I2Cx);
+	return data;
+}
+/* This funtion issues a stop condition and therefore
+* releases the bus
+*/
+void I2C_stop(I2C_TypeDef* I2Cx)
+{
+	// Send I2C1 STOP Condition
+	I2C_GenerateSTOP(I2Cx, ENABLE);
+}
+void I2C_slave_start(I2C_TypeDef* I2Cx)
+{
+	/* Test on I2C1 EV1 and clear it */
+	while(!I2C_CheckEvent(I2Cx, I2C_EVENT_SLAVE_RECEIVER_ADDRESS_MATCHED));
+}
+
+/* This funtion issues a stop condition and therefore
+* releases the bus
+*/
+void I2C_slave_stop(I2C_TypeDef* I2Cx)
+{
+	/* Test on I2C2 EV4 and clear it */
+	while(!I2C_CheckEvent(I2Cx, I2C_EVENT_SLAVE_STOP_DETECTED));
+	/* Clear I2C2 STOPF flag: read operation to I2C_SR1 followed by a
+	write operation to I2C_CR1 */
+	(void)(I2C_GetFlagStatus(I2Cx, I2C_FLAG_STOPF));
+	I2C_Cmd(I2Cx, ENABLE);
+}
+/* This function reads one byte from the slave device
+* and acknowledges the byte (requests another byte)
+*/
+uint8_t I2C_slave_read_ack(I2C_TypeDef* I2Cx)
+{
+	/* Test on I2C2 EV2 and clear it */
+	while(!I2C_CheckEvent(I2Cx, I2C_EVENT_SLAVE_BYTE_RECEIVED));
+	uint8_t data = I2C_ReceiveData(I2Cx);
+	return data;
+}
+/* This function reads one byte from the slave device
+* and doesn't acknowledge the recieved data
+*/
+uint8_t I2C_slave_read_nack(I2C_TypeDef* I2Cx)
+{
+	/* Test on I2C2 EV2 and clear it */
+	while(!I2C_CheckEvent(I2Cx, I2C_EVENT_SLAVE_BYTE_RECEIVED));
+	uint8_t data = I2C_ReceiveData(I2Cx);
+	return data;
+}
+
+void init_I2C1(void)
+{
+	#define I2C1_SLAVE_ADDRESS7 0x90
+
+	GPIO_InitTypeDef GPIO_InitStruct;
+	I2C_InitTypeDef I2C_InitStruct;
+	// enable APB1 peripheral clock for I2C1
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1, ENABLE);
+	// enable clock for SCL and SDA pins
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+	/* setup SCL and SDA pins
+	* You can connect I2C1 to two different
+	* pairs of pins:
+	* 1. SCL on PB6 and SDA on PB7
+	* 2. SCL on PB8 and SDA on PB9
+	*/
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_7 | GPIO_Pin_8; // we are going to use PB6 and PB7
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF; // set pins to alternate function
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz; // set GPIO speed
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_OD; // set output to open drain --> the line has to be only pulled low, not driven high
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP; // enable pull up resistors
+	GPIO_Init(GPIOB, &GPIO_InitStruct); // init GPIOB
+	// Connect I2C1 pins to AF
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource8, GPIO_AF_I2C1); // SCL
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource7, GPIO_AF_I2C1); // SDA
+	// configure I2C1
+	I2C_InitStruct.I2C_ClockSpeed = 100000; // 100kHz
+	I2C_InitStruct.I2C_Mode = I2C_Mode_I2C; // I2C mode
+	I2C_InitStruct.I2C_DutyCycle = I2C_DutyCycle_2; // 50% duty cycle --> standard
+	I2C_InitStruct.I2C_OwnAddress1 = I2C1_SLAVE_ADDRESS7; // own address, not relevant in master mode
+	I2C_InitStruct.I2C_Ack = I2C_Ack_Enable; // disable acknowledge when reading (can be changed later on)
+	I2C_InitStruct.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit; // set address length to 7 bit addresses
+	I2C_Init(I2C1, &I2C_InitStruct); // init I2C1
+	// enable I2C1
+	I2C_Cmd(I2C1, ENABLE);
+}
+
+uint16_t temperature;
+
+void vExpansionBoardI2CTemperatureTask(void* pvparameters)
+{
+	#define MPC9800_ADDRESS 0x90 // adres MCP9800
+	//#define I2C_Speed 200000
+
+
+	init_I2C1(); // na podstawie: http://eliaselectronics.com/stm32f4-tutorials/stm32f4-i2c-mastertutorial/
+	I2C_start(I2C1, MPC9800_ADDRESS, I2C_Direction_Transmitter);
+	I2C_write(I2C1,0x01); // ustaw adres rejestru Configuration Register
+	I2C_write(I2C1,0x30); // ustaw rozdzielczosc na 12 bit
+	I2C_stop(I2C1); // stop the transmission
+	I2C_start(I2C1, MPC9800_ADDRESS, I2C_Direction_Transmitter);
+	I2C_write(I2C1,0x00); // ustaw adres rejestru z temperatur¹
+	I2C_stop(I2C1); // stop the transmission
+
+	uint8_t lowByte;
+	uint8_t highByte;
+
+	for(;;)
+	{
+		I2C_start(I2C1, MPC9800_ADDRESS, I2C_Direction_Receiver); // start a transmission in Master receiver mode
+		highByte = I2C_read_ack(I2C1); // read one byte and request another byte
+		lowByte = I2C_read_nack(I2C1); // read one byte and don't request another byte
+		I2C_stop(I2C1); // stop the transmission
+		temperature = ((uint16_t)highByte << 4) + ((uint16_t)lowByte >> 4);
+
+		vTaskDelay(1000 / portTICK_RATE_MS);
+	}
+}
+
+void USART_write(uint8_t data)
+{
+	// Send a char...
+	USART_SendData(USART3, data);
+
+	// ... wait until the char is sended
+	while(USART_GetFlagStatus(USART3, USART_FLAG_TXE) == RESET)
+		asm("nop");
+}
+
+void vExpansionBoardUSARTTask(void* pvparameters)
+{
+	//NVIC_InitTypeDef NVIC_InitStructure;
+
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+	GPIO_InitTypeDef GPIO_InitStructure;
+	/* Configure USART2_Tx and USART2_Rx as alternate function */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource8, GPIO_AF_USART3);
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource9, GPIO_AF_USART3);
+
+	/* Enable USART3 clocks */
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
+
+	// enable oversampling by 8 for higher speeds
+	USART_OverSampling8Cmd(USART3, ENABLE);
+
+	/* USART3 configuration ------------------------------------------------------*/
+	 /* USART3 configured as follow:
+		   - BaudRate = 460,8k baud
+		   - Word Length = 8 Bits
+		   - One Stop Bit
+		   - No parity
+		   - Hardware flow control - none
+		   - Receive and transmit enabled
+	 */
+	USART_InitTypeDef USART_InitStructure;
+	USART_InitStructure.USART_BaudRate = 115200;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No ;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+	USART_Init(USART3, &USART_InitStructure);
+
+	/* Enable the USART3 Interrupt */
+	//NVIC_InitStructure.NVIC_IRQChannel = USART3_IRQn;
+	//NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x0F;
+	//NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x0F;
+	//NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	//NVIC_Init(&NVIC_InitStructure);
+
+	/* Enable the USART3 */
+	USART_Cmd(USART3, ENABLE);
+
+	//USART_ITConfig(USART3, USART_IT_RXNE, ENABLE);
+	//USART_ITConfig(USART3, USART_IT_TC, ENABLE);
+
+	for(;;)
+	{
+		USART_write('h');
+		USART_write('e');
+		USART_write('l');
+		USART_write('l');
+		USART_write('o');
+		USART_write(' ');
+		uint8_t temperature8bits = (temperature >> 4);
+		USART_write(temperature8bits);
+		USART_write(' ');
+		vTaskDelay(1000 / portTICK_RATE_MS);
+	}
 }
 
 static volatile uint8_t MainBoardLedChange = 1;
